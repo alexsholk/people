9dc43c11f6406b1c6873b02e9e1efd88c48ee235
Aleksandr Sholk <Aleksandr.Sholk@InToSoft.by>
2019-08-22 16:27:35 +0300

    Start developing intopeople theme

---
+++ wp-content/themes/intopeople/head.php
@@ -0,0 +1,6 @@
+<head>
+	<meta charset="UTF-8">
+	<title>InToPeople</title>
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
+	<link href="/wp-content/themes/intopeople/style.css" rel="stylesheet">
+</head>
