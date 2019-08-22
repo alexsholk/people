75656da8b60049032cc35f23a1599da3619fcf34
Aleksandr Sholk <Aleksandr.Sholk@InToSoft.by>
2019-08-22 16:51:46 +0300

    Developing intopeople theme...

--- wp-content/themes/intopeople/footer.php
+++ wp-content/themes/intopeople/footer.php
@@ -1,10 +1,12 @@
-<div class="footer">
-	<div class="main-wrap">
-		<div class="logo"></div>
-		<div class="social-links">
-			<a href="#" class="twitter"></a>
-			<a href="#" class="facebook"></a>
-			<a href="#" class="linkedin"></a>
-		</div>
-	</div>
-</div>
+    <div class="footer">
+        <div class="main-wrap">
+            <div class="logo"></div>
+            <div class="social-links">
+                <a href="#" class="twitter"></a>
+                <a href="#" class="facebook"></a>
+                <a href="#" class="linkedin"></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- wp-content/themes/intopeople/index.php
+++ wp-content/themes/intopeople/index.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<?php get_template_part('head') ?>
-<?php //require '_head.php' ?>
-<body>
 <?php get_header() ?>
-<?php //require '_header.php' ?>
 <div class="main-container">
 	<?php ///require '_intro.php' ?>
 	<?php //require '_clients.php' ?>
 	<?php //require '_contact_form.php' ?>
 </div>
-<?php get_footer() ?>
-<?php //require '_footer.php' ?>
-</body>
-</html>
+<?php get_footer();
