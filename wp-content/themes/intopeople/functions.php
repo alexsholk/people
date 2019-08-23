8b42659ff47d31279e007b897838d2a6970f7cdd
Aleksandr Sholk <Aleksandr.Sholk@InToSoft.by>
2019-08-23 17:39:41 +0300

    Developing intopeople theme...

---
+++ wp-content/themes/intopeople/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+if ( ! function_exists( 'intopeople_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which runs
+	 * before the init hook. The init hook is too late for some features, such as indicating
+	 * support post thumbnails.
+	 */
+	function intopeople_setup() {
+		/**
+		 * Make theme available for translation.
+		 * Translations can be placed in the /languages/ directory.
+		 */
+		load_theme_textdomain( 'intopeople', get_template_directory() . '/languages' );
+
+		/**
+		 * Add support for two custom navigation menus.
+		 */
+		register_nav_menus( array(
+			'top'    => __( 'Top Menu', 'intopeople' ),
+			'social' => __( 'Social Links Menu', 'intopeople' ),
+		) );
+	}
+endif; // intopeople_setup
+
+add_action( 'after_setup_theme', 'intopeople_setup' );
--- wp-content/themes/intopeople/index.php
+++ wp-content/themes/intopeople/index.php
@@ -1,5 +1,15 @@
 <?php get_header() ?>
 <div class="main-container">
+	<?php
+        if ( have_posts() ) :
+            while ( have_posts() ) :
+                the_post();
+	            the_title( '<h2>', '</h2>' );
+	            the_content();
+                // Display post content
+            endwhile;
+        endif;
+	?>
 	<?php ///require '_intro.php' ?>
 	<?php //require '_clients.php' ?>
 	<?php //require '_contact_form.php' ?>
