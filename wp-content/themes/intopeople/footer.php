ef3eba76086892c209de7f2dbf3b690c38e87d83
Aleksandr Sholk <Aleksandr.Sholk@InToSoft.by>
2019-08-26 13:09:40 +0300

    Developing intopeople theme (social links)

--- wp-content/themes/intopeople/footer.php
+++ wp-content/themes/intopeople/footer.php
@@ -1,11 +1,7 @@
     <div class="footer">
         <div class="main-wrap">
             <div class="logo"></div>
-            <div class="social-links">
-                <a href="#" class="twitter"></a>
-                <a href="#" class="facebook"></a>
-                <a href="#" class="linkedin"></a>
-            </div>
+	        <?php wp_nav_menu( array( 'theme_location' => 'social', 'container' => false, 'menu_class' => 'social-links' ) ) ?>
         </div>
     </div>
     <?php wp_footer() ?>
